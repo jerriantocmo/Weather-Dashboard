09ddefe779c9ac05020bdb6a2415a4c2840e0774
jerriantocmo <jerriant94@gmail.com>
2026-06-23 05:35:07 -0700

    Removed uneeded import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
-import { getWeather } from './api'
-import Card from './components/Card'
 import DailyWeatherCard from './components/DailyWeatherCard'
 import CurrentWeatherCard from './components/CurrentWeatherCard'
 import HourlyWeatherCard from './components/HourlyWeatherCard'
@@ -9,10 +7,6 @@ import HourlyWeatherCard from './components/HourlyWeatherCard'
 
 function App() {
 
-  const {data} = useQuery({
-    queryKey: ['weather'],
-    queryFn: () => getWeather({lat: 0, lon: 50, timeline: '1day'})
-  })
   return (
     <div className='flex flex-col gap-4'>
       <CurrentWeatherCard />
