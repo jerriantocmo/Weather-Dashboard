70cca3ddb7281527bb02b673fde06c856fb7cf6e
jerriantocmo <jerriant94@gmail.com>
2026-06-23 05:34:09 -0700

    Refacored api.ts to fix type errors

--- src/api.ts
+++ src/api.ts
@@ -1,21 +1,29 @@
-import { currentWeatherResponseSchema, dailyWeatherResponseSchema, hourlyWeatherResponseSchema } from "./schemas/weatherschema"
+import { currentWeatherResponseSchema } from "./schemas/currentWeatherSchema"
+import { dailyWeatherResponseSchema } from "./schemas/dailyWeatherSchema"
+import { hourlyWeatherResponseSchema } from "./schemas/hourlyWeatherSchema"
 
 const API_KEY = import.meta.env.VITE_API_KEY
 
- export async function getWeather({lat, lon, timeline}: {lat: number, lon: number, timeline: 'current' | '1h' | '1day'}){
+export async function getWeatherCurrent({ lat, lon }: { lat: number, lon: number }) {
+    const res = await fetch(`https://api.openweathermap.org/data/4.0/onecall/current?lat=${lat}&lon=${lon}&units=imperial&excludes=minutely,alerts&lang=en&appid=${API_KEY}`)
 
-    const endpointPath = timeline ==='current' ? '/current' : `/timeline/${timeline}`
-    
-    const res = await fetch(`https://api.openweathermap.org/data/4.0/onecall${endpointPath}?lat=${lat}&lon=${lon}&units=imperial&excludes=minutely,alerts&lang=en&appid=${API_KEY}`)
-    
     const data = await res.json()
-    
-    switch(timeline){
-        case 'current':
-            return currentWeatherResponseSchema.parse(data)
-        case '1h':
-            return hourlyWeatherResponseSchema.parse(data)
-        case '1day':
-            return dailyWeatherResponseSchema.parse(data)
-    }
- }
+
+    return currentWeatherResponseSchema.parse(data)
+
+}
+export async function getWeatherHourly({ lat, lon }: { lat: number, lon: number }) {
+    const res = await fetch(`https://api.openweathermap.org/data/4.0/onecall/timeline/1h?lat=${lat}&lon=${lon}&units=imperial&excludes=minutely,alerts&lang=en&appid=${API_KEY}`)
+
+    const data = await res.json()
+
+    return hourlyWeatherResponseSchema.parse(data)
+}
+
+export async function getWeatherDaily({ lat, lon }: { lat: number, lon: number }) {
+    const res = await fetch(`https://api.openweathermap.org/data/4.0/onecall/timeline/1day?lat=${lat}&lon=${lon}&units=imperial&excludes=minutely,alerts&lang=en&appid=${API_KEY}`)
+
+    const data = await res.json()
+
+    return dailyWeatherResponseSchema.parse(data)
+}
